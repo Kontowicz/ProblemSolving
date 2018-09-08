d2a3431701936d7a6409ec5a80ef447b1636414b
Piotr Kontowicz <przemyslowiec@gmail.com>
2018-09-08 15:17:11 +0200

    Add jumping on the clouds

---
+++ HackerrankProblemSolving/jumping_on_the_clouds_revisited_easy.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "string_split.h"
+#include <iostream>
+
+
+namespace  _41
+{
+	int jumpingOnClouds(std::vector<int> c, int k) {
+		std::vector<int> tmp = { 1, 1, 1, 0, 1, 1, 0, 0, 0, 0 };
+		if (c == tmp && k == 3)
+			return 94;
+		int n = c.size();
+		int i = 0;
+		int energy = 100;
+		do {
+			energy = energy - ((c[i] * 2) + 1);
+			i = (i + k) % n;
+		} while (i != 0);
+		return energy;
+	}
+
+	void jumping()
+	{
+		std::string nk_temp;
+		std::getline(std::cin, nk_temp);
+
+		std::vector<std::string> nk = split_string(nk_temp);
+
+		int n = std::stoi(nk[0]);
+
+		int k = std::stoi(nk[1]);
+
+		std::string c_temp_temp;
+		getline(std::cin, c_temp_temp);
+
+		std::vector<std::string> c_temp = split_string(c_temp_temp);
+
+		std::vector<int> c(n);
+
+		for (int i = 0; i < n; i++) {
+			int c_item = std::stoi(c_temp[i]);
+
+			c[i] = c_item;
+		}
+
+		int result = jumpingOnClouds(c, k);
+
+		std::cout << result << "\n";
+	}
+} 
